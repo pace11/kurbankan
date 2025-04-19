f7535153e4e19e1ffca84081cf7206bc1b650b2f
pace11 <ryanjoker87@gmail.com>
2025-04-19 22:52:20 +0900

    chore: adding .env file and .gitignore

--- config/db.go
+++ config/db.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,11 +12,21 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:rootpassword@tcp(127.0.0.1:3306)/kurbankan?parseTime=true"
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		dbUser, dbPass, dbHost, dbPort, dbName,
+	)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
+
 	DB = db
 	fmt.Println("Connected to MySQL")
 }
--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"kurbankan/config"
 	"kurbankan/routes"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load .env
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
 	r := gin.Default()
 	config.ConnectDB()
 	routes.SetupRoutes(r)
-	r.Run(":4000")
+	r.Run(os.Getenv("APP_PORT"))
 }
