56ada63b75d62a5fdb224f6a8f630df73f795754
pace11 <ryanjoker87@gmail.com>
2026-05-11 01:23:30 +0700

    feat: update dependencies and add CORS middleware for improved API security

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,22 @@ import (
 	"kurbankan/controllers"
 	"kurbankan/middlewares"
 	"kurbankan/repository"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// controllers
 	QurbanPeriodController := controllers.NewQurbanPeriodController(repository.NewQurbanPeriodRepository())
